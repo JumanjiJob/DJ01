d7928a9f7dbee4bc6e938bfd01934e441db8ce32
JumanjiJob <35mirzhan@mail.ru>
2025-01-22 11:00:07 +0300

    Done

---
+++ DJ01/main/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return HttpResponse("<h1>Это мой первый проект на Django</h1>")
+
+def new(request):
+    return HttpResponse("<h1>Это вторая страница проекта на Django</h1>")
+
+def data(request):
+    return HttpResponse("<h1>Содержимое страницы data</h1>")
+
+def test(request):
+    return HttpResponse("<h1>Содержимое страницы test</h1>")
