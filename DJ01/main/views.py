22eba845ec19195c3f7367cada0acbef9c339cb3
JumanjiJob <35mirzhan@mail.ru>
2025-01-24 10:51:05 +0300

    Update views.py

--- DJ01/main/views.py
+++ DJ01/main/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1>Это мой первый проект на Django</h1>")
+    return HttpResponse("<h1>Это мой ghfпервый проект на Django</h1>")
 
 def new(request):
     return HttpResponse("<h1>Это вторая страница проекта на Django</h1>")
